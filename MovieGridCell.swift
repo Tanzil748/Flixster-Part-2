1392ad2914e4ae2fc9b1f4ae1e2dfe3b0a88ff52
Tanzil748 <76973558+Tanzil748@users.noreply.github.com>
2021-07-19 00:40:37 -0400

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Tanzil Hassan on 7/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
